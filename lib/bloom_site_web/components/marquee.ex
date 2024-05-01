5240de5bd580e21cdb554bfbaff21a3d468d7527
Chris Gregori <chrisgreg23@googlemail.com>
2024-05-01 19:22:30 +0100

    Add card and marquee

--- lib/bloom_site_web/components/marquee.ex
+++ lib/bloom_site_web/components/marquee.ex
@@ -1,11 +1,43 @@
 defmodule BloomSiteWeb.Components.Marquee do
   use Phoenix.Component
 
-  attr(:pause_on_hover, :boolean, default: false)
-  attr(:repeat, :integer, default: 4)
-  attr(:vertical, :boolean, default: false)
-  attr(:reverse, :boolean, default: false)
-  attr(:class, :string, default: "")
+  @moduledoc """
+  Marquee component
+
+  Sliding carousel of images that can go backwards and forwards.
+
+  Requires the following Tailwind CSS config extension:
+
+  extend: {
+      animation: {
+        marquee: "marquee 20s linear infinite",
+        "marquee-vertical": "marquee-vertical 20s linear infinite",
+      },
+      keyframes: {
+        tilt: {
+          "0%, 50%, 100%": { transform: "rotate(0deg)" },
+          "25%": { transform: "rotate(6deg)" },
+          "75%": { transform: "rotate(-6deg)" },
+        },
+        marquee: {
+          from: { transform: "translateX(0)" },
+          to: { transform: "translateX(calc(-100% - var(--gap)))" },
+        },
+        "marquee-vertical": {
+          from: { transform: "translateY(0)" },
+          to: { transform: "translateY(calc(-100% - var(--gap)))" },
+        },
+      },
+    }
+  """
+
+  attr(:pause_on_hover, :boolean, default: false, doc: "Pause on hover")
+  attr(:repeat, :integer, default: 4, doc: "Number of repeats")
+  attr(:vertical, :boolean, default: false, doc: "Vertical")
+  attr(:reverse, :boolean, default: false, doc: "Reverse")
+  attr(:class, :string, default: "", doc: "CSS class for parent div")
+
+  attr(:rest, :global)
 
   slot(:inner_block, required: true)
 
@@ -26,6 +58,7 @@ defmodule BloomSiteWeb.Components.Marquee do
         !@vertical && "flex-row",
         @class
       ]}
+      {@rest}
     >
       <%= for _ <- 0..(@repeat - 1) do %>
         <div
